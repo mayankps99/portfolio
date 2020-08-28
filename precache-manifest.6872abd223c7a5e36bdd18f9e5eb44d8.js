7416d83400cc8b0d6406130a9e2cd1f76a9b8471
mayankps99 <mayank.bbs1998@gmail.com>
2020-08-28 15:40:52 +0530

    Updates

---
+++ precache-manifest.6872abd223c7a5e36bdd18f9e5eb44d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c2200c1763865f0532d0f2e986e2b8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "de641080cd348bea9ffa",
+    "url": "/portfolio/static/css/main.b3cbdeae.chunk.css"
+  },
+  {
+    "revision": "fcfa8f3d280c36770615",
+    "url": "/portfolio/static/js/2.12afb408.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.12afb408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de641080cd348bea9ffa",
+    "url": "/portfolio/static/js/main.7b8b3a8e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e125dd0a95e1ab284c9b9c6e514fc5d7",
+    "url": "/portfolio/static/media/svg-2.e125dd0a.svg"
+  },
+  {
+    "revision": "63f1bf2bf856a31896f1cb9066a87782",
+    "url": "/portfolio/static/media/svg-4.63f1bf2b.svg"
+  },
+  {
+    "revision": "069927142d79355d7ddf14fbc280a7bf",
+    "url": "/portfolio/static/media/svg-6.06992714.svg"
+  },
+  {
+    "revision": "ecdaaec500f1740d99f1538844853740",
+    "url": "/portfolio/static/media/svg-7.ecdaaec5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1b9e50633795a6c902f3201baac2ca07.js"
+  "/portfolio/precache-manifest.6872abd223c7a5e36bdd18f9e5eb44d8.js"
 );
 
 self.addEventListener('message', (event) => {
