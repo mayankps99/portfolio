c458354f0eda8b41648a3132479bfddbdcc7c0e3
mayankps99 <mayank.bbs1998@gmail.com>
2020-08-28 13:59:58 +0530

    Updates

---
+++ precache-manifest.1b9e50633795a6c902f3201baac2ca07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fc18f5924853fd3b14ef53e2a92055",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "69f2392f36e2d317e7f5",
+    "url": "/portfolio/static/css/main.c69d866e.chunk.css"
+  },
+  {
+    "revision": "d0ce332f84dc830979ca",
+    "url": "/portfolio/static/js/2.7d69ed34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.7d69ed34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f2392f36e2d317e7f5",
+    "url": "/portfolio/static/js/main.ed8fe0fe.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
